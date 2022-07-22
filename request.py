640baafef87d49c98fbebfba1990e99aef59dad4
Rajusambangi <102754205+Rajusambangi@users.noreply.github.com>
2022-07-22 23:09:41 +0530

    Delete request.py

--- request.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'SepalLengthCm':5.1, 'SepalWidthCm':3.5, 'PetalLengthCm':1.4,'PetalWidthCm':0.2})
-
-print(r.json())
